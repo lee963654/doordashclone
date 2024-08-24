1ad29d60a698d2f0512adb0d941b10d4a0f6e5a7
James Lee <lee963654@gmail.com>
2024-08-24 15:18:31 -0400

    added isprod variable

--- backend/app.js
+++ backend/app.js
@@ -5,3 +5,6 @@ const cors = require('cors');
 const csurf = require('csurf');
 const helmet = require('helmet');
 const cookieParser = require('cookie-parser');
+
+const { environment } = require('./config');
+const isProduction = environment === 'production';
