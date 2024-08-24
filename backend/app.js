de7b692cfdcb9d6ceba9046befd455106d968bce
James Lee <lee963654@gmail.com>
2024-08-24 15:19:35 -0400

    notes on the app file

--- backend/app.js
+++ backend/app.js
@@ -10,3 +10,5 @@ const { environment } = require('./config');
 const isProduction = environment === 'production';
 
 const app = express();
+
+// Phase 0 Just did initialize the express application. Will connect the morgan middleware next
