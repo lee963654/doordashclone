3c9ecfc7072cae6cc19c5a6abb2ce9d6b0e4911c
James Lee <lee963654@gmail.com>
2024-09-10 17:52:16 -0400

    moved app.use down the file

--- backend/app.js
+++ backend/app.js
@@ -9,39 +9,39 @@ const cookieParser = require('cookie-parser');
 const { environment } = require('./config');
 const isProduction = environment === 'production';
 
-const app = express();
 const routes = require('./routes');
+const app = express();
 
 app.use(morgan('dev'));
 
 app.use(cookieParser());
 app.use(express.json());
-app.use(routes);
 
 // Security Middleware
 if (!isProduction) {
-    // enable cors only in development
-    app.use(cors());
-  }
+  // enable cors only in development
+  app.use(cors());
+}
 
-  // helmet helps set a variety of headers to better secure your app
-  app.use(
-    helmet.crossOriginResourcePolicy({
-      policy: "cross-origin"
-    })
-  );
+// helmet helps set a variety of headers to better secure your app
+app.use(
+  helmet.crossOriginResourcePolicy({
+    policy: "cross-origin"
+  })
+);
 
-  // Set the _csrf token and create req.csrfToken method
-  app.use(
-    csurf({
-      cookie: {
-        secure: isProduction,
-        sameSite: isProduction && "Lax",
-        httpOnly: true
-      }
-    })
-  );
+// Set the _csrf token and create req.csrfToken method
+app.use(
+  csurf({
+    cookie: {
+      secure: isProduction,
+      sameSite: isProduction && "Lax",
+      httpOnly: true
+    }
+  })
+);
 
 
+app.use(routes);
 
 module.exports = app;
