f56299f1e05c6f38ee98cd72a7fef121d495dca9
James Lee <lee963654@gmail.com>
2024-10-23 16:33:34 -0400

    adding the rest of the imports to the auth file

--- backend/utils/auth.js
+++ backend/utils/auth.js
@@ -1,3 +1,5 @@
 // backend/utils/auth.js
 const jwt = require('jsonwebtoken');
 const { jwtConfig } = require('../config');
+
+// Phase 3, User Auth Middlewares, just created auth file in the utils folder. Will now add const {user} above.
