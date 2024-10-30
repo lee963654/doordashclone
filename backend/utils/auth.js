7835e338f4612efe86c7d0aa90a241db4a67a84e
James Lee <lee963654@gmail.com>
2024-10-30 13:34:48 -0400

    added user

--- backend/utils/auth.js
+++ backend/utils/auth.js
@@ -1,5 +1,6 @@
 // backend/utils/auth.js
 const jwt = require('jsonwebtoken');
 const { jwtConfig } = require('../config');
+const { User } = require('../db/models');
 
 // Phase 3, User Auth Middlewares, just created auth file in the utils folder. Will now add const {user} above.
