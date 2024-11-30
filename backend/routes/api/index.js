180ee33b49252fa973bf2ca9712c872455d04852
James Lee <lee963654@gmail.com>
2024-11-30 15:16:32 -0500

    added route for set token

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -10,4 +10,16 @@ router.post('/test', function(req, res) {
 const { setTokenCookie } = require('../../utils/auth.js');
 const { User } = require('../../db/models');
 
+
+router.get('/set-token-cookie', async (_req, res) => {
+  const user = await User.findOne({
+    where: {
+      username: 'Demo-lition'
+    }
+  });
+  setTokenCookie(res, user);
+  return res.json({ user: user });
+});
+
+
 module.exports = router;
