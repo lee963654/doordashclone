034b896322adb2e00f7fd7d7b9aef43fef1b32b1
James Lee <lee963654@gmail.com>
2024-12-05 10:45:40 -0500

    added router get restore user

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -26,5 +26,11 @@ router.get('/set-token-cookie', async (_req, res) => {
 const { restoreUser } = require('../../utils/auth.js');
 router.use(restoreUser);
 
+router.get(
+  '/restore-user',
+  (req, res) => {
+    return res.json(req.user);
+  }
+);
 
 module.exports = router;
