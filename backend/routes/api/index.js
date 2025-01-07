57771cc36eb1674b71d9a8af8855a99615a75018
James Lee <lee963654@gmail.com>
2025-01-07 16:55:09 -0500

    added usersrouter

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,6 +1,7 @@
 // backend/routes/api/index.js
 const router = require('express').Router();
 const sessionRouter = require('./session.js');
+const usersRouter = require('./users.js');
 
 router.post('/test', function(req, res) {
     res.json({ requestBody: req.body });
